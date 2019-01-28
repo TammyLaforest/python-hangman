610b283bb30ec571be3df81288e71e1d75776ac5
TammyLaforest <TammyLaforest@gmail.com>
2019-01-28 13:42:18 -0500

    added Rock Paper Scissors

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session
+from flask import Flask, render_template, request, redirect, url_for,  make_response, session
 from flask_session.__init__ import Session
 from flask.views import MethodView
 from tempfile import mkdtemp
@@ -18,9 +18,269 @@ Session(app)
 def index():
 	return render_template("index.html")
 
+# Rock, Paper, Scissors
+
+choices = ['rock', 'paper', 'scissors']
+
 @app.route('/rps')
 def rps():
-	return render_template("rps.html")
+	session.clear()
+	session["humanscore"] = 0
+	session["aiscore"] = 0
+	message="Choose Rock, Paper or Scissors to Start Game"
+	return render_template("rps.html", message=message, humanscore=session["humanscore"], aiscore=session["aiscore"])
+
+# session["humanscore"] = 0
+# session["aiscore"] = 0
+
+@app.route("/rps/<choice>")
+def playrps(choice):
+
+	session["aichoice"] = random.choice(choices)
+	print(choice)
+	aichoice = session["aichoice"]
+	session["humanchoice"] = choice
+	print(aichoice)
+
+	if aichoice == choice:
+		print("this thing")
+		session["message"]="Draw!"
+
+	elif session["aichoice"] == 'rock':
+		if choice == "paper":
+			session["message"]= "You win!"
+			session["humanscore"] +=1
+		else:
+			session["message"]= "You lose! Try again."
+			session["aiscore"]+=1
+	
+	elif session["aichoice"] == 'paper':
+		if choice == "scissors":
+			session["message"]= "You win!"
+			session['humanscore'] +=1
+		else:
+			session["message"]= "You lose! Try again."
+			session['aiscore']+=1
+	
+	else:
+		if choice == 'rock':
+			session["message"] = "You win!"
+			session['humanscore']+=1
+		else:
+			session["message"]= "You lose! Try again."
+			session['aiscore']+=1
+	
+	message=session["message"]
+	humanscore=session["humanscore"]
+	aiscore=session["aiscore"]
+	humanchoice=session["humanchoice"]
+
+	if aiscore > humanscore:
+		winning="red"
+	else:
+		winning="black" 
+
+	# return render_template("rps.html",  aichoice=aichoice, humanchoice=humanchoice)
+
+	return render_template("rps.html", message=message, humanscore=humanscore, aiscore=aiscore, aichoice=aichoice, humanchoice=humanchoice, winning=winning)
+
+
+# Tic Tac Toe
+
+@app.route("/tictactoe_home")
+def tictactoe_home():
+		session.clear()
+		session["won"] = True
+		session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+		session["turn"] = "X"
+		session["message"] = "Start a New Game"
+
+		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won = session["won"] )
+ 
+@app.route("/new_game")
+def new_game():
+		session.clear()
+		session["message"] = "Begin! X goes first."
+		session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+		session["turn"] = "X"
+		session["won"] = False
+		session["count"] = 0
+
+		board = session["board"]
+		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won = session["won"] )
+
+
+@app.route("/play/<int:row>/<int:col>")
+def play(row, col):
+	session["board"][row][col] = session["turn"]
+	turn = session ["turn"]
+	board = session["board"]
+	session["message"] = score(row, col)
+	if session["message"] == "Draw!":
+		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
+
+	# Change the turn
+	if session["turn"] == "X":
+		session["turn"] = "Y"
+	else: 
+		session["turn"] = "X"
+	session["count"] += 1
+	print(session["count"])
+	return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
+
+
+def score(row, col):
+		board = session["board"]
+		turn = session ["turn"]
+		
+		if (board[row][0] == board[row][1] == board[row][2] 
+			or board[0][col] == board[1][col] == board[2][col]):
+			session["won"] = True
+			return f"{ turn } wins!"
+			
+		elif board[1][1] is not None:
+			if (board[0][0] == board[1][1] == board[2][2] 
+			or board[2][0] == board[1][1] == board[0][2]):
+				if board[1][1] == turn:
+					session["won"] = True
+					return f" {turn } Wins!"
+			
+			elif session["count"] == 9:
+				return "Draw!"
+			else:
+				return "Try Again!"
+
+		else:
+			if turn == "X":
+				return "It is Y's turn."
+			else:
+				return "Your move!"
+
+
+@app.route("/ai_game")
+def ai_game():
+		session.clear()
+		session["message"] = "New game. Human goes first."
+		session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+		session["scores"] = [[0, 0, 0,], [0, 0, 0], [0, 0, 0]]
+		session["turn"] = "X"
+		session["won"] = False
+		session["count"] = 0
+
+		board = session["board"]
+		return render_template("playai_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won = session["won"] )
+
+
+@app.route("/playai/<int:row>/<int:col>")
+def playai(row, col):
+	
+	session["board"][row][col] = session["turn"]
+
+	session["scores"][row][col] = 1
+	session["count"] += 1
+	session["message"] = score(row, col)
+	if session["message"] == "Draw!":
+		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
+
+	
+	session["turn"] = "Y"
+	
+	row, col = computer_choice()
+	session["board"][row][col] = session["turn"]
+	session["scores"][row][col] = -3
+	session["count"] += 1
+	
+	session["message"] = score(row, col)
+	if session["message"] == "Draw!":
+		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
+
+	
+	session["turn"] = "X" 
+
+	return render_template("playai_tictactoe.html", game=session["board"], message=session["message"], won =session["won"])
+
+
+
+def computer_choice():
+	board = session["board"]
+	scores = session ["scores"]
+	turn = session ["turn"]
+	corners = (0, 2)
+	
+
+	# If player one doesn't choose the center in their first move, choose the center. Otherwise choose a corner. 
+	if session["count"] == 1:
+		if board[1][1] is None:
+			return 1, 1
+		else:
+			return random.choice(corners), random.choice(corners)
+	
+	# If there is a chance of playing a winning move, play that first. 
+	# Check for horizontal wins. 
+	else:
+		for i in range(3):
+			if sum(scores[i]) == -6:
+				for j in range(3):
+					if board[i][j] != "Y":
+						return i, j
+
+	# Check for vertical wins.
+		for j in range(3):
+			if (scores[0][j] + scores[1][j] + scores[2][j]) == -6:
+				for i in range(3):
+					if board[i][j] != "Y":
+						return i, j
+
+	# Check for first diagonal win. 
+		if scores[0][0] + scores[1][1] + scores[2][2] == -6:
+			if board[0][0] != "Y":
+				return  0, 0
+			else:
+				return 2, 2
+
+	# Check for second diagonal win. 
+		elif scores[0][2] + scores[1][1] + scores[2][0] == -6:
+			if board[0][2] != "Y":
+				return 0, 2
+			else:
+				return 2, 0		
+
+	# If a win isn't an option, check if there is the immediate threat of defeat.
+	# Check for horizontal threats. 
+		for i in range(3):
+			if sum(scores[i]) == 2:
+				for j in range(3):
+					if board[i][j] != "X":
+						return i, j
+
+	# Check for vertical threats.
+		for j in range(3):
+			if (scores[0][j] + scores[1][j] + scores[2][j]) == 2:
+				for i in range(3):
+					if board[i][j] != "X":
+						return i, j
+
+	# Check for first diagonal threat. 
+		if scores[0][0] + scores[1][1] + scores[2][2] == 2:
+			if board[0][0] != "X":
+				return  0, 0
+			else:
+				return 2, 2
+	
+	# Check for second diagonal threat. 
+		elif scores[0][2] + scores[1][1] + scores[2][0] == 2:
+			if board[0][2] != "X":
+				return 0, 2
+			else:
+				return 2, 0		
+
+	# Else, find an empty spot and take it. 
+		else: 
+			for i in range(3):
+				for j in range(3):
+					if board[i][j] != "X" and board[i][j] != "Y":
+						return  i, j
+
 
 # Hangman
 
@@ -183,206 +443,6 @@ def check(word,guesses):
 def you_win(joined_list):
 	global message
 	message=('{} is it! It took {} tries.'.format(word, len(guesses)))
-
-
-
-
-# Tic Tac Toe
-
-@app.route("/tictactoe_home")
-def tictactoe_home():
-		session.clear()
-		session["won"] = True
-		session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
-		session["turn"] = "X"
-		session["message"] = "Start a New Game"
-
-		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won = session["won"] )
- 
-@app.route("/new_game")
-def new_game():
-		session.clear()
-		session["message"] = "Begin! X goes first."
-		session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
-		session["turn"] = "X"
-		session["won"] = False
-		session["count"] = 0
-
-		board = session["board"]
-		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won = session["won"] )
-
-
- 
-@app.route("/play/<int:row>/<int:col>")
-def play(row, col):
-	session["board"][row][col] = session["turn"]
-	turn = session ["turn"]
-	board = session["board"]
-	session["message"] = score(row, col)
-	if session["message"] == "Draw!":
-		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
-
-	# Change the turn
-	if session["turn"] == "X":
-		session["turn"] = "Y"
-	else: 
-		session["turn"] = "X"
-	session["count"] += 1
-	print(session["count"])
-	return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
-
-
-def score(row, col):
-		board = session["board"]
-		turn = session ["turn"]
-		
-		if (board[row][0] == board[row][1] == board[row][2] 
-			or board[0][col] == board[1][col] == board[2][col]):
-			session["won"] = True
-			return f"{ turn } wins!"
-			
-		elif board[1][1] is not None:
-			if (board[0][0] == board[1][1] == board[2][2] 
-			or board[2][0] == board[1][1] == board[0][2]):
-				if board[1][1] == turn:
-					session["won"] = True
-					return f" {turn } Wins!"
-			
-			elif session["count"] == 8:
-				return "Draw!"
-			else:
-				return "Try Again!"
-
-		else:
-			if turn == "X":
-				return "It is Y's turn."
-			else:
-				return "Your move!"
-
-
-@app.route("/ai_game")
-def ai_game():
-		session.clear()
-		session["message"] = "New game. Human goes first."
-		session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
-		session["scores"] = [[0, 0, 0,], [0, 0, 0], [0, 0, 0]]
-		session["turn"] = "X"
-		session["won"] = False
-		session["count"] = 0
-
-		board = session["board"]
-		return render_template("playai_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won = session["won"] )
-
-
-@app.route("/playai/<int:row>/<int:col>")
-def playai(row, col):
-	
-	session["board"][row][col] = session["turn"]
-
-	session["scores"][row][col] = 1
-	session["count"] += 1
-	session["message"] = score(row, col)
-	if session["message"] == "Draw!":
-		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
-
-	
-	session["turn"] = "Y"
-	
-	row, col = computer_choice()
-	session["board"][row][col] = session["turn"]
-	session["scores"][row][col] = -3
-	session["count"] += 1
-	
-	session["message"] = score(row, col)
-	if session["message"] == "Draw!":
-		return render_template("play_tictactoe.html", game=session["board"], turn=session["turn"], message=session["message"], won =session["won"])
-
-	
-	session["turn"] = "X" 
-
-	return render_template("playai_tictactoe.html", game=session["board"], message=session["message"], won =session["won"])
-
-
-
-def computer_choice():
-	board = session["board"]
-	scores = session ["scores"]
-	turn = session ["turn"]
-	corners = (0, 2)
-	
-
-	# If player one doesn't choose the center in their first move, choose the center. Otherwise choose a corner. 
-	if session["count"] == 1:
-		if board[1][1] is None:
-			return 1, 1
-		else:
-			return random.choice(corners), random.choice(corners)
-	
-	# If there is a chance of playing a winning move, play that first. 
-	# Check for horizontal wins. 
-	else:
-		for i in range(3):
-			if sum(scores[i]) == -6:
-				for j in range(3):
-					if board[i][j] != "Y":
-						return i, j
-
-	# Check for vertical wins.
-		for j in range(3):
-			if (scores[0][j] + scores[1][j] + scores[2][j]) == -6:
-				for i in range(3):
-					if board[i][j] != "Y":
-						return i, j
-
-	# Check for first diagonal win. 
-		if scores[0][0] + scores[1][1] + scores[2][2] == -6:
-			if board[0][0] != "Y":
-				return  0, 0
-			else:
-				return 2, 2
-
-	# Check for second diagonal win. 
-		elif scores[0][2] + scores[1][1] + scores[2][0] == -6:
-			if board[0][2] != "Y":
-				return 0, 2
-			else:
-				return 2, 0		
-
-	# If a win isn't an option, check if there is the immediate threat of defeat.
-	# Check for horizontal threats. 
-		for i in range(3):
-			if sum(scores[i]) == 2:
-				for j in range(3):
-					if board[i][j] != "X":
-						return i, j
-
-	# Check for vertical threats.
-		for j in range(3):
-			if (scores[0][j] + scores[1][j] + scores[2][j]) == 2:
-				for i in range(3):
-					if board[i][j] != "X":
-						return i, j
-
-	# Check for first diagonal threat. 
-		if scores[0][0] + scores[1][1] + scores[2][2] == 2:
-			if board[0][0] != "X":
-				return  0, 0
-			else:
-				return 2, 2
-	
-	# Check for second diagonal threat. 
-		elif scores[0][2] + scores[1][1] + scores[2][0] == 2:
-			if board[0][2] != "X":
-				return 0, 2
-			else:
-				return 2, 0		
-
-	# Else, find an empty spot and take it. 
-		else: 
-			for i in range(3):
-				for j in range(3):
-					if board[i][j] != "X" and board[i][j] != "Y":
-						return  i, j
 
 
 
